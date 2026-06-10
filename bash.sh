51214f3fd013b6f1b78222a2aa5e2ee77ce153a2
Kavibalan1904 <124227120+Kavibalan1904@users.noreply.github.com>
2026-06-10 10:08:16 +0530

    Delete bash.sh

--- bash.sh
+++
@@ -1,7 +0,0 @@
-cd reso-bot
-git init
-git add .
-git commit -m "Initial commit: Reso music bot"
-git branch -M main
-git remote add origin https://github.com/YOUR_USERNAME/reso-bot.git
-git push -u origin main
